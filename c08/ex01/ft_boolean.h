89d3b918a5b22ec2444cd3ceffe33fea4fc768c1
Lemma-Barnabas <solomon25td@gmail.com>
2022-09-29 03:45:55 -0400

    final version

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,13 @@
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+# include <unistd.h>
+
+typedef char	t_bool;
+
+# define FALSE 0
+# define TRUE 1
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+# define EVEN(X) ((X % 2) == 1 ? 0 : 1)
+#endif
---
+++ c08/ex02/ft_abs.h
@@ -0,0 +1,4 @@
+#ifndef FT_ABS_H
+# define FT_ABS_H
+# define ABS(X) ((X < 0) ? -X : X)
+#endif
